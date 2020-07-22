4c4a81d83e272a065a62bd406c068994b289d5dd
DHRUV-EDDI <dhruv2018570@gmail.com>
2020-07-22 23:52:36 +0530

    Init Commit

---
+++ public/js/chat.js
@@ -0,0 +1,100 @@
+const socket = io();
+
+// Elements
+const FormTag = document.querySelector('#message-form');
+const inputTag = FormTag.querySelector('input');
+const submitButton = FormTag.querySelector('button')
+const sendLocationButton = document.querySelector('#sendLocation')
+const messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
+
+// Options
+const { username, room } = Qs.parse(location.search,{ ignoreQueryPrefix: true});
+
+//AutoScroll
+const autoScroll = () => {
+    //new message element
+    const newMessage = messages.lastElementChild
+    //Height of the new message
+    const newMessageStyles = getComputedStyle(newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = messages.offsetHeight + newMessageMargin
+    // Visible Height
+    const visibleHeight = messages.offsetHeight
+    // Height of messages container
+    const containerHeight = messages.scrollHeight
+    //How far have I scrolled?
+    const scrollOffset = messages.scrollTop + visibleHeight
+
+    if((containerHeight - newMessageHeight) <= scrollOffset)
+        messages.scrollTop = messages.scrollHeight
+}
+socket.on('message',(message) => {
+    console.log(message);
+    const html = Mustache.render(messageTemplate,{
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('hh:mm a')
+    })
+    messages.insertAdjacentHTML("beforeend",html)
+    autoScroll()
+})
+
+socket.on('locationMessage',(message) => {
+    console.log(message)
+    const html = Mustache.render(locationTemplate,{
+        username: message.username,
+        url : message.url,
+        createdAt : moment(message.createdAt).format('hh:mm a')
+    })
+    messages.insertAdjacentHTML("beforeend",html)
+    autoScroll()
+})
+FormTag.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    // disable the submit Button
+    submitButton.setAttribute('disabled','disabled')
+    // console.log(e);
+    const message = e.target.elements.message.value ;
+    socket.emit('sendMessage',message,(error) => {
+        //enable the submit Button
+        submitButton.removeAttribute('disabled')
+        inputTag.value = ''
+        inputTag.focus()
+        if(error)
+            return console.log(error);
+        console.log('Delivered!');
+    });
+
+});
+
+sendLocationButton.addEventListener('click',() => {
+    // Here we are using browser API to grab the lattitude and longitude
+    // Note: Old browsers won't support geolocation feature
+    if(!navigator.geolocation)
+        return alert('Browser not supports this feature');
+    //disable the button while fetching the geolocation
+    sendLocationButton.setAttribute('disabled','disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation',{
+            lattitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        },() => {
+            // enable the Button
+            sendLocationButton.removeAttribute('disabled')
+            
+            console.log('Location shared!')
+        })
+    })
+})
+
+socket.emit('join',{ username, room },(error) => {
+    if(error){
+        alert(error);
+        location.href = '/'
+    }
+})
---
+++ src/index.js
@@ -0,0 +1,60 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUserInRoom } = require('./utils/users.js')
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(`${__dirname}/../public`));
+
+io.on('connection',(socket) => {
+    console.log('New client connected...');
+    
+    socket.on('join',(options,callback) => {
+        // console.log(username, room)
+        const {error, user} = addUser({ id:socket.id, ...options})
+        
+        if(error)
+            return callback(error)
+
+        socket.join(user.room)
+        // This emits an event to only connected client
+        socket.emit('message',generateMessage('Admin','Welcome!'))
+
+        // This emits an event to all connected clients except the Parent client
+        // To specify the room we used .to() method
+        socket.broadcast.to(user.room).emit('message',generateMessage('Admin',`${user.username} has joined!`));
+        callback();
+    })
+    // This emits an event to all connected clients (io.emit => Listener)
+    socket.on('sendLocation',(coords,callback) => {        
+        const user = getUser(socket.id);
+        io.to(user.room).emit('locationMessage',generateLocationMessage(user.username,`https://www.google.com/maps?q=${coords.lattitude},${coords.longitude}`));
+        callback()
+    })
+    socket.on('sendMessage',(message,callback) => {
+        const user = getUser(socket.id)
+        const filter = new Filter()
+        
+        if(filter.isProfane(message))
+            return callback('Profanity is not allowed');
+        
+        io.to(user.room).emit('message',generateMessage(user.username,message));
+        callback();
+    })
+
+    // Built-in event triggers when a connected client left
+    socket.on('disconnect',() => {
+        const user = removeUser(socket.id)
+        if(user)
+            io.to(user.room).emit('message',generateMessage('Admin',`${user.username} has left!`))
+    })
+})
+server.listen(port,() => {
+    console.log(`Listening at port ${port}....`);
+})
